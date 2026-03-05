b18dfaa9767dc5d65c98f25bbc14328b493eacf0
Tinku679 <tinkupayal7575@gmail.com>
2026-03-05 11:28:43 +0530

    first program in java

---
+++ src/first_program.java
@@ -0,0 +1,15 @@
+
+public class first_program {
+    public static void main(String[] args) {
+        int n = 10;
+        int first =0 ;
+        int second =1;
+        System.out.println(first + " " + second + " ");
+        for (int i=2;i<n;i++){
+            int next = first + second;
+            System.out.println(next);
+            first = second;
+            second = next;
+        }
+    }
+}
